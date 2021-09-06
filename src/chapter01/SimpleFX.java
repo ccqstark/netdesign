63b436369d27b03298b6a3dcf80abebc219d9680
ccqstark <1367305698@qq.com>
2021-09-06 12:17:36 +0800

    差一点

--- src/chapter01/SimpleFX.java
+++ src/chapter01/SimpleFX.java
@@ -1,6 +1,7 @@
 package chapter01;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,6 +9,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
@@ -15,6 +19,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
+
+import static javafx.scene.input.KeyCode.*;
 
 
 public class SimpleFX extends Application {
@@ -29,6 +35,9 @@ public class SimpleFX extends Application {
   //显示信息的文本区域
   private TextArea taDisplay = new TextArea();
 
+  // 快捷键
+//  private KeyCombination shift_enter = new KeyCombination(KeyCode.X,KeyCombination.CONTROL_DOWN);
+
 
   public static void main(String[] args) {
     launch(args);
@@ -38,10 +47,12 @@ public class SimpleFX extends Application {
   public void start(Stage primaryStage) {
 
     // 事件
-    btnExit.setOnAction(event -> {System.exit(0);});
+    btnExit.setOnAction(event -> {
+      System.exit(0);
+    });
     btnSend.setOnAction(event -> {
       String msg = tfSend.getText();
-      taDisplay.appendText(msg+"\n");
+      taDisplay.appendText(msg + "\n");
       tfSend.clear();
     });
     TextFileIO textFileIO = new TextFileIO();
@@ -50,13 +61,25 @@ public class SimpleFX extends Application {
         LocalDateTime.now().withNano(0) + " " + taDisplay.getText()
       );
     });
-    btnOpen.setOnAction(event ->{
+    btnOpen.setOnAction(event -> {
       String msg = textFileIO.load();
-      if(msg!=null){
+      if (msg != null) {
         taDisplay.clear();
         taDisplay.setText(msg);
       }
     });
+
+    // 回车监听
+    tfSend.setOnKeyPressed((KeyEvent keyEvent)-> {
+        if (keyEvent.getCode() == ENTER)  {
+          String msg = tfSend.getText();
+          taDisplay.appendText(msg + "\n");
+          tfSend.clear();
+        }
+      }
+    );
+
+
 
     BorderPane mainPane = new BorderPane();
     //内容显示区域
@@ -66,9 +89,11 @@ public class SimpleFX extends Application {
     vBox.setPadding(new Insets(10, 20, 10, 20));
     vBox.getChildren().addAll(new Label("信息显示区："),
       taDisplay, new Label("信息输入区："), tfSend);
+
     //设置显示信息区的文本区域可以纵向自动扩充范围
     VBox.setVgrow(taDisplay, Priority.ALWAYS);
     mainPane.setCenter(vBox);
+
     //底部按钮区域
     HBox hBox = new HBox();
     hBox.setSpacing(10);
@@ -77,6 +102,21 @@ public class SimpleFX extends Application {
     hBox.getChildren().addAll(btnSend, btnSave, btnOpen, btnExit);
     mainPane.setBottom(hBox);
     Scene scene = new Scene(mainPane, 700, 400);
+
+    scene.set
+    scene.getAccelerators().put(new KeyCombination() {
+      @Override
+      public boolean match(KeyEvent event) {
+        return super.match(event);
+      }
+    }, new Runnable() {
+      public void run() {
+        String msg = tfSend.getText();
+        taDisplay.appendText("echo:" + msg + "\n");
+        tfSend.clear();
+      }
+    });
+
     primaryStage.setScene(scene);
     primaryStage.show();
 
