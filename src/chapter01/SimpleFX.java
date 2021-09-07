4e3faff20ffa4514f4efe3211859eb9ad3433606
ccqstark <1367305698@qq.com>
2021-09-07 11:59:19 +0800

    KeyCodeCombination

--- src/chapter01/SimpleFX.java
+++ src/chapter01/SimpleFX.java
@@ -1,6 +1,7 @@
 package chapter01;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -8,6 +9,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -17,10 +21,9 @@ import javafx.stage.Stage;
 
 import java.time.LocalDateTime;
 
-import static javafx.scene.input.KeyCode.ENTER;
-
-
 public class SimpleFX extends Application {
+
+    final KeyCombination kb = new KeyCodeCombination(KeyCode.ENTER, KeyCombination.SHIFT_DOWN);
 
     private final Button btnExit = new Button("退出");
     private final Button btnSend = new Button("发送");
@@ -62,13 +65,13 @@ public class SimpleFX extends Application {
             }
         });
 
-        // 回车监听
+        // 快捷键监听
         tfSend.setOnKeyPressed((KeyEvent keyEvent) -> {
-                    if (keyEvent.getCode() == ENTER && keyEvent.isShiftDown()) {
+                    if (kb.match(keyEvent)) {
                         String msg = tfSend.getText();
                         taDisplay.appendText("echo: " + msg + "\n");
                         tfSend.clear();
-                    } else if (keyEvent.getCode() == ENTER) {
+                    } else if (keyEvent.getCode() == KeyCode.ENTER) {
                         String msg = tfSend.getText();
                         taDisplay.appendText(msg + "\n");
                         tfSend.clear();
@@ -98,6 +101,16 @@ public class SimpleFX extends Application {
         hBox.getChildren().addAll(btnSend, btnSave, btnOpen, btnExit);
         mainPane.setBottom(hBox);
         Scene scene = new Scene(mainPane, 700, 400);
+
+        // 组合快捷键
+//        scene.getAccelerators().put(
+//                KeyCombination.keyCombination("SHIFT+ENTER"),
+//                () -> {
+//                    String msg = tfSend.getText();
+//                    taDisplay.appendText("echo: " + msg + "\n");
+//                    tfSend.clear();
+//                }
+//        );
 
         primaryStage.setScene(scene);
         primaryStage.show();
