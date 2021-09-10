172ff1389860c35a0b3a7051be93b4840f04af93
ccqstark <1367305698@qq.com>
2021-09-10 14:43:33 +0800

    feat:chapter02预习完成

---
+++ src/chapter02/TCPClient.java
@@ -0,0 +1,54 @@
+package chapter02;
+
+import java.io.*;
+import java.net.Socket;
+
+public class TCPClient {
+
+    private Socket socket; // 定义套接字
+    // 定义字符输入流和输出流
+    private PrintWriter pw;
+    private BufferedReader br;
+
+    public TCPClient(String ip, String port) throws IOException {
+        // 主动向服务器发起连接，实现TCP的三次握手过程
+        // 如果不成功，则抛出错误信息，其错误信息交由调用者处理
+        socket = new Socket(ip, Integer.parseInt(port));
+
+        // 得到网络输出字节流地址，并封装成网络输出字符流
+        OutputStream socketOut = socket.getOutputStream();
+        pw = new PrintWriter(new OutputStreamWriter(socketOut, "utf-8"), true);
+
+        InputStream socketIn = socket.getInputStream();
+        br = new BufferedReader(new InputStreamReader(socketIn, "utf-8"));
+    }
+
+    public void send(String msg) {
+        pw.println(msg);
+    }
+
+    public String receive() {
+        String msg = null;
+        try {
+            // 从网络输入字节流中读信息，每次只能接受一行信息
+            // 如果不够一行（无行结束服务），则该语句阻塞等待
+            // 直到条件满足，程序才往下运行
+            msg = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return msg;
+    }
+
+    public void close() {
+        try {
+            if (socket != null){
+                // 关闭socket连接及相关的输入流输出流，实现四次握手断开
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/chapter02/TCPClientFX.java
@@ -0,0 +1,149 @@
+package chapter02;
+
+import chapter01.TextFileIO;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class TCPClientFX extends Application {
+
+    private final Button btnExit = new Button("退出");
+    private final Button btnSend = new Button("发送");
+    private final Button btnOpen = new Button("加载");
+    private final Button btnSave = new Button("保存");
+    private final Button btnConnect = new Button("连接");
+
+    //待发送信息的文本框
+    private final TextField tfSend = new TextField();
+    //显示信息的文本区域
+    private final TextArea taDisplay = new TextArea();
+    // IP地址输入框
+    private final TextField ipInput = new TextField();
+    // 端口输入框
+    private final TextField portInput = new TextField();
+
+    // TCP客户端
+    private TCPClient tcpClient;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void exitSocket() {
+        if (tcpClient != null) {
+            tcpClient.send("bye");
+            tcpClient.close();
+        }
+        System.exit(0);
+    }
+
+    public void sendSocketMsg() {
+        String sendMsg = tfSend.getText();
+        tcpClient.send(sendMsg);
+        taDisplay.appendText("客户端发送: " + sendMsg + "\n");
+
+        String receiveMsg = tcpClient.receive();
+        taDisplay.appendText(receiveMsg + "\n");
+        tfSend.clear();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // 事件
+        btnExit.setOnAction((event) -> {
+                    exitSocket();
+                }
+        );
+        btnSend.setOnAction((event) -> {
+            sendSocketMsg();
+        });
+        TextFileIO textFileIO = new TextFileIO();
+        btnSave.setOnAction((event) ->
+                textFileIO.append(
+                        LocalDateTime.now().withNano(0) + " " + taDisplay.getText()
+                )
+        );
+        btnOpen.setOnAction(event -> {
+            String msg = textFileIO.load();
+            if (msg != null) {
+                taDisplay.clear();
+                taDisplay.setText(msg);
+            }
+        });
+
+        // 快捷键监听
+        tfSend.setOnKeyPressed((KeyEvent keyEvent) -> {
+                    if (keyEvent.getCode() == KeyCode.ENTER) {
+                        sendSocketMsg();
+                    }
+                }
+        );
+
+        // 连接按钮
+        btnConnect.setOnAction(event -> {
+            String ip = ipInput.getText().trim();
+            String port = portInput.getText().trim();
+            try {
+                tcpClient = new TCPClient(ip, port);
+                // 成功连接服务器接收欢迎信息
+                String firstMsg = tcpClient.receive();
+                taDisplay.appendText(firstMsg + "\n");
+            } catch (IOException e) {
+                taDisplay.appendText("服务器连接失败!" + e.getMessage() + "\n");
+            }
+        });
+
+        BorderPane mainPane = new BorderPane();
+
+        // ip和端口输入区
+        FlowPane flowPane = new FlowPane();
+        flowPane.setHgap(10);
+        flowPane.setVgap(20);
+        flowPane.setPadding(new Insets(15, 15, 15, 15));
+        flowPane.getChildren().addAll(new Label("IP地址:"), ipInput, new Label("端口:"), portInput, btnConnect);
+        mainPane.setTop(flowPane);
+
+        //内容显示区域
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);//各控件之间的间隔
+        //VBox面板中的内容距离四周的留空区域
+        vBox.setPadding(new Insets(10, 20, 10, 20));
+        vBox.getChildren().addAll(new Label("信息显示区："),
+                taDisplay, new Label("信息输入区："), tfSend);
+
+        //设置显示信息区的文本区域可以纵向自动扩充范围
+        VBox.setVgrow(taDisplay, Priority.ALWAYS);
+        mainPane.setCenter(vBox);
+
+        //底部按钮区域
+        HBox hBox = new HBox();
+        hBox.setSpacing(10);
+        hBox.setPadding(new Insets(10, 20, 10, 20));
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        hBox.getChildren().addAll(btnSend, btnSave, btnOpen, btnExit);
+        mainPane.setBottom(hBox);
+        Scene scene = new Scene(mainPane, 700, 400);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.setOnCloseRequest(event -> {
+            exitSocket();
+        });
+
+        primaryStage.show();
+    }
+
+}
